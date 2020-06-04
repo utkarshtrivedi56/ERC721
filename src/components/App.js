964d4715b608f269a43926b3a0b95c658af48549
Utkarsh Trivedi <utkarshtrivedi56@gmail.com>
2020-06-04 19:02:13 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -16,6 +16,7 @@ class App extends Component {
     if(window.ethereum){
       window.web3 = new Web3(window.ethereum);
       await window.ethereum.enable();
+      ethereum.autoRefreshOnNetworkChange = false;
     }
     else if (window.web3){
       window.web3 = new Web3(window.web3.currentProvider);
